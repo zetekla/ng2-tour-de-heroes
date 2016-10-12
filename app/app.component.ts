60988cf6d99b8e2c46e9dbec659513ec6e901fe8
Phuc <zenithtekla@gmail.com>
2016-10-12 10:22:07 -0700

    r1 finished part1

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,16 +7,21 @@ export class Hero {
 
 @Component({
     selector: 'my-app',
-    template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>
+    template: `
+      <h1>{{title}}</h1>
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name">
+      </div>
     `
 })
 export class AppComponent {
+  title = 'Tour of Heroes';
 
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   };
-
-  title = 'Tour of Heroes';
 }
