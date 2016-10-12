4fc0f700d7f4e8a1ac6ebe4f76279b82db11c238
Phuc <zenithtekla@gmail.com>
2016-10-12 10:17:59 -0700

    r0 initial touch on appComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: `<h1>{{title}}</h1><h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div><div><label>name: </label>{{hero.name}}</div>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  title = 'Tour of Heroes';
+}
