bc9a1bc035443176239de026bea08c574c2c8f1c
Phuc <zenithtekla@gmail.com>
2016-10-12 10:58:14 -0700

    r3 p3 reusable component, accept input
    Let’s take stock of what we’ve built.
    
    We created a reusable component
    We learned how to make a component accept input
    We learned to declare the application directives we need in an Angular module. We list the directives in the NgModule decorator's declarations array.
    We learned to bind a parent component to a child component.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
 import {FormsModule} from "@angular/forms";
+import {HeroDetailComponent} from "./hero-detail.component";
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeroDetailComponent ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
